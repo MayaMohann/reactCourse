2fdf242432cc20d45a72a67e3c8048228f33d0f7
MayaMohann <mayamohan2212@gmail.com>
2024-09-09 19:22:51 +0530

    FetchUsingHook

---
+++ Custom_Hook/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import  './Components/Fetchdata'
+import Fetchdata from './Components/Fetchdata'
+import FetchYogaData from './Components/FetchYogaData'
+function App() {
+
+  return (
+    <>
+    <Fetchdata/>
+    <FetchYogaData/>
+    </>
+  )
+}
+
+export default App
