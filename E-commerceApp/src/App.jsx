8d715f36f40149908628b5c139e8f457004d10ff
MayaMohann <mayamohan2212@gmail.com>
2024-09-11 13:33:57 +0530

    first commit

--- E-commerceApp/src/App.jsx
+++ E-commerceApp/src/App.jsx
@@ -1,10 +1,16 @@
 
 import './App.css'
+import ProductList from './Components/ProductList'
+import ShoppingCart from './Components/ShoppingCart'
+// import SuperCoins from './Components/SuperCoin'
 
 function App() {
 
   return (
     <>
+    <ProductList/>
+    <ShoppingCart/>
+    {/* <SuperCoins/> */}
     </>
   )
 }
